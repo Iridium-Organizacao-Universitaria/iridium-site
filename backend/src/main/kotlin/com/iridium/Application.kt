8273b58b24f525c536d4edc2f112bbff56bb140d
beavct <beatrizvianacosta16@gmail.com>
2024-05-13 14:47:39 -0300

    Subi os arquivos de classe e teste que a May já havia feito.

--- backend/src/main/kotlin/com/iridium/Application.kt
+++ backend/src/main/kotlin/com/iridium/Application.kt
@@ -3,6 +3,8 @@ package com.iridium
 import com.iridium.plugins.*
 import io.ktor.server.application.*
 import io.ktor.server.plugins.cors.routing.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
 
 /*
 fun main(args: Array<String>) {
@@ -10,18 +12,22 @@ fun main(args: Array<String>) {
 }
  */
 
+
 fun main() {
     embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
         .start(wait = true)
 }
 
 fun Application.module() {
+    /*
+    como já instalou o plugin, não precisa instalar dnv (erro de pacote duplicado)
     install(CORS) {
         anyHost()
     }
-    configureHTTP()
+     */
+    //configureHTTP()
     configureSerialization()
-    configureDatabases()
-    configureMonitoring()
+    //configureDatabases()
+    //configureMonitoring()
     configureRouting()
 }
--- backend/src/test/kotlin/com/iridium/ApplicationTest.kt
+++ backend/src/test/kotlin/com/iridium/ApplicationTest.kt
@@ -1,4 +1,4 @@
-package com.iridium
+/*package com.iridium
 
 import com.iridium.plugins.*
 import io.ktor.client.request.*
@@ -17,5 +17,42 @@ class ApplicationTest {
             assertEquals(HttpStatusCode.OK, status)
             assertEquals("Hello World!", bodyAsText())
         }
+    }
+}
+ */
+
+import com.iridium.models.*
+import com.iridium.routes.*
+
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+import io.ktor.http.*
+import io.ktor.server.routing.*
+import io.ktor.server.testing.*
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import kotlin.test.assertContains
+
+class ApplicationTest {
+    @Test
+    fun testRoot() = testApplication {
+//        application {
+//            module()
+//        }
+        val response = client.get("/")
+
+        assertEquals(HttpStatusCode.OK, response.status)
+        assertEquals("Iridium - Organização Universitária", response.bodyAsText())
+    }
+
+    @Test
+    fun testNewEndpoint() = testApplication {
+//    application {
+//        module()
+//    }
+        val response = client.get("/test1")
+        assertEquals(HttpStatusCode.OK, response.status)
+        assertEquals("html", response.contentType()?.contentSubtype)
+        assertContains(response.bodyAsText(), "Página de test1")
     }
 }
