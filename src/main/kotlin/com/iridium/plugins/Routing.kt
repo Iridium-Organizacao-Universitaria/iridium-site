fe1cf66c0f2a07999b6c30b0f48e00d6ad833a83
Maysa <maysaclaudino21@gmail.com>
2024-05-13 16:19:12 -0300

    Dá pra navegar nas páginas

--- src/main/kotlin/com/iridium/plugins/Routing.kt
+++ src/main/kotlin/com/iridium/plugins/Routing.kt
@@ -4,10 +4,34 @@ import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
+import com.iridium.routes.*
+import io.ktor.http.* // ContentType
+import io.ktor.server.http.content.* // staticResources
+//import io.ktor.server.plugins.statuspages.*
+
 fun Application.configureRouting() {
     routing {
-        get("/") {
-            call.respondText("Hello World!")
+        disciplinaRouting()
+        atividadeRouting()
+
+        // Cria paginas da pasta mycontent no endpoint content (link.com/content/sample.html)
+        // a pasta mycontent deve estar em src/main/resources
+        staticResources("/", "frontend")
+
+//        get("/") {
+//            call.respondText("Iridium - Organização Universitária")
+//        }
+
+        // Adiciona uma nova pagina (link.com/test1)
+        get("/test1") {
+            val text = "<h1>Página de test1</h1>"
+            val type = ContentType.parse("text/html")
+            call.respondText(text, type)
+        }
+
+        // Uma pagina para o plugin Status Pages nao entendi o objetivo
+        get("/error-test") {
+            throw IllegalStateException("Too Busy")
         }
     }
 }
