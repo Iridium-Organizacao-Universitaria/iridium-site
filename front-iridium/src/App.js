e2bcb133457bf1dbbafd972e74d6526e5d6d5de8
beavct <beatrizvianacosta16@gmail.com>
2024-07-02 01:21:30 -0300

    tinha esquecido de colocar o token no App.js

--- front-iridium/src/App.js
+++ front-iridium/src/App.js
@@ -1,5 +1,3 @@
-// src/App.js
-
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
 import QuemSomos from './components/qmsomos/QuemSomos';
@@ -16,19 +14,19 @@ const AuthChecker = () => {
     const { token, setToken } = useToken();
     const location = useLocation();
 
-    if((!token) && location.pathname !== '/registro/Registro' && location.pathname !== '/') {
+    if (!token && location.pathname !== '/registro/Registro' && location.pathname !== '/') {
         return <Login setToken={setToken} />;
     }
 
     return (
         <Routes>
-            <Route path="/atividade_ind/:atividadeName" element={<Atividade token={token}/>} />
-            <Route path="/disciplina_ind/:disciplinaName" element={<Disciplina token={token}/>} />
-            <Route path="/atividades/Atividades" element={<Atividades token={token}/>} />
-            <Route path="/disciplinas/Disciplinas" element={<Disciplinas token={token}/>} />
-            <Route path="/login/Login" element={<Login setToken={setToken}/>} />
+            <Route path="/atividade_ind/:atividadeName" element={<Atividade token={token} />} />
+            <Route path="/disciplina_ind/:disciplinaName" element={<Disciplina token={token} />} />
+            <Route path="/atividades/Atividades" element={<Atividades token={token} />} />
+            <Route path="/disciplinas/Disciplinas" element={<Disciplinas token={token} />} />
+            <Route path="/login/Login" element={<Login setToken={setToken} />} />
             <Route path="/registro/Registro" element={<Registro />} />
-            <Route path="/perfil/Perfil" element={<Perfil token={token}/>} />
+            <Route path="/perfil/Perfil" element={<Perfil token={token} />} />
             <Route path="/" element={<QuemSomos />} />
         </Routes>
     );
@@ -43,32 +41,5 @@ function App() {
         </Router>
     );
 }
-
-// function App() {
-//     const { token, setToken } = useToken();
-//     const location = useLocation();
-//
-//     // console.log("Token = ", token);
-//     if (!token && location.pathname !== '/registro/Registro') {
-//         return <Login setToken={setToken} />;
-//     }
-//
-//     return (
-//         <Router>
-//             <div>
-//                 <Routes>
-//                     <Route path="/atividade_ind/:atividadeName" element={<Atividade />} />
-//                     <Route path="/disciplina_ind/:disciplinaName" element={<Disciplina />} />
-//                     <Route path="/atividades/Atividades" element={<Atividades />} />
-//                     <Route path="/disciplinas/Disciplinas" element={<Disciplinas />} />
-//                     <Route path="/login/Login" element={<Login />} />
-//                     <Route path="/registro/Registro" element={<Registro />} />
-//                     <Route path="/perfil/Perfil" element={<Perfil />} />
-//                     <Route path="/" element={<QuemSomos />} />
-//                 </Routes>
-//             </div>
-//         </Router>
-//     );
-// }
 
 export default App;
